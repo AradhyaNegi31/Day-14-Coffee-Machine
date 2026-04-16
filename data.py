994e25381e233e7bde1a709e67ea0267c42c8745
Aradhya31 <aradhyanegi3105@gmail.com>
2026-04-16 19:06:35 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,31 @@
+Menue ={
+    "espresso":{
+        "ingredient":{
+            "water":50,
+            "coffee":18,
+        },
+        "cost":1.5,
+    },
+    "latte":{
+        "ingredient":{
+            "water":200,
+            "milk":150,
+            "coffee":24,
+        },
+        "cost":2.5,
+    },
+    "cappuccino":{
+        "ingredient":{
+            "water":250,
+            "milk":100,
+            "coffee":24,
+        },
+        "cost":3.0,
+    },
+}
+
+Resources ={
+    "water":1000,
+    "milk":2000,
+    "coffee":1000,
+}
